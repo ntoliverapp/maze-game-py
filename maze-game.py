20803db4996190ba3bcbe0e35e28edf723ae05f6
ntoliverapp <n.toliverapplication@gmail.com>
2021-10-16 01:39:07 -0700

    first commit

---
+++ maze-game.py
@@ -0,0 +1,14 @@
+#Part 1: Setting up the Maze
+import turtle
+#create a window, turtle module, screen method
+wn = turtle.Screen()
+wn.bgcolor('black')
+wn.title('A Maze Game')
+wn.setup(700, 700)
+
+#Create Pen
+class Pen(turtle.Turtle):
+    
+
+
+
